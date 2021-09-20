33ed958a2c12c7431a8146014c090d03ad1cf297
SimonLiu0911 <milkjhang@icloud.com>
2021-09-20 17:15:25 +0800

    complete Login page

---
+++ src/store/utils.js
@@ -0,0 +1,102 @@
+import Vue from 'vue';
+
+// Bootstrap-notify(http://bootstrap-growl.remabledesigns.com)
+export function notifyAlert(msg, type, icon, title, url, target) {
+  if (type === '' || type === undefined) {
+    type = 'danger';
+  }
+  if (icon === '' || icon === undefined) {
+    switch (type) {
+      case 'success':
+        icon = 'fas fa-check-circle';
+        break;
+      case 'info':
+        icon = 'fas fa-info-circle';
+        break;
+      case 'warning':
+        icon = 'fas fa-exclamation-triangle';
+        break;
+      case 'danger':
+        icon = 'fas fa-times-circle';
+        break;
+      default:
+        icon = 'fas fa-question-circle';
+        break;
+    }
+  }
+
+  window.$.notify(
+    // options
+    {
+      icon: icon,
+      title: title,
+      message: msg,
+      url: url,
+      target: target
+    },
+    // settings
+    {
+      element: 'body',
+      position: null,
+      type: type,
+      allow_dismiss: true,
+      newest_on_top: false,
+      showProgressbar: false,
+      placement: {
+        from: 'top',
+        align: 'center'
+      },
+      offset: 20,
+      spacing: 10,
+      z_index: 9999,
+      delay: 5000,
+      timer: 1000,
+      url_target: '_blank',
+      mouse_over: null,
+      animate: {
+        enter: 'animated fadeInDown',
+        exit: 'animated fadeOutUp'
+      },
+      onShow: null,
+      onShown: null,
+      onClose: null,
+      onClosed: null,
+      icon_type: 'class',
+      template:
+        '<div data-notify="container" class="col-11 col-sm-6 col-lg-4 alert alert-{0}" role="alert">' +
+        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+        '<i data-notify="icon"></i> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<div class="progress" data-notify="progressbar">' +
+        '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+        '</div>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+        '</div>'
+    }
+  );
+};
+
+/**
+ * Ajax other settings.
+ * post/patch: { 'method': 'post or patch', 'url': '/api/...', 'data': params }
+ * get: { 'method': 'get', 'url': '/api/...', 'params': urlParams }
+ * @param {*} config
+ * @param {*} nextFn  下一步驟執行的function
+ */
+export const vueAjaxSubmit = new Vue({
+  methods: {
+    ajaxSubmit(config, nextFn) {
+      this.$http(config)
+        .then(response => {
+          if (response.status) {
+            // 下一步驟執行的function
+            nextFn(response.data);
+          }
+        })
+        .catch(error => {
+          notifyAlert(error.response.data.errors[0], 'danger');
+        });
+    }
+  }
+});
--- src/views/Login/Index.vue
+++ src/views/Login/Index.vue
@@ -1,15 +1,87 @@
 <script>
+import * as utils from '@/store/utils';
+
 export default {
   components: {
-    Layout: () => import('@layout/main')
+    // Layout: () => import('@layout/main')
+  },
+  data() {
+    return {
+      user: {
+        email: '',
+        password: ''
+      },
+      isLoading: false
+    };
+  },
+  methods: {
+    // TODO
+    signIn() {
+      utils.notifyAlert('Success to sign in!', 'success');
+      console.log('signIn');
+    }
   }
 };
 </script>
 
 <template>
-  <Layout>
-    <section>
-      Login
-    </section>
-  </Layout>
+  <!-- <Layout> -->
+  <div id="login">
+    <div class="container">
+      <div class="row">
+        <div class="col-4 m-auto">
+          <form class="form-signin p-3 m-auto" @submit.prevent="signIn">
+            <h1 class="h3 mb-3 font-weight-normal text-center">Sign in</h1>
+            <div class="form-group">
+              <label for="inputEmail" class="sr-only">Email address</label>
+              <input
+                id="inputEmail"
+                type="email"
+                class="form-control rounded-0"
+                :class="$style.input_style"
+                placeholder="Email address"
+                v-model="user.email"
+                autofocus
+              />
+            </div>
+            <div class="form-group">
+              <label for="inputPassword" class="sr-only">Password</label>
+              <input
+                id="inputPassword"
+                type="password"
+                class="form-control rounded-0"
+                :class="$style.input_style"
+                placeholder="Password"
+                v-model="user.password"
+              />
+            </div>
+            <button
+              class="btn btn-lg btn-dark btn-block rounded-0"
+              type="submit"
+            >
+              Sign In
+            </button>
+            <button
+              class="btn btn-lg btn-outline-dark btn-block rounded-0"
+              type="submit"
+            >
+              Go Back to Front Page
+            </button>
+            <p class="my-3 text-muted text-center">&copy; 2020~∞ - 練習用</p>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- Vue Loading -->
+    <Loading :active.sync="this.isLoading"></Loading>
+  </div>
+  <!-- </Layout> -->
 </template>
+
+<style lang="scss" module>
+.input_style:focus {
+  border-style: solid;
+  border-color: gray;
+  box-shadow: 0 0 3px gray;
+}
+</style>
